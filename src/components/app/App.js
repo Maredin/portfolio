982dec386fafc97f52d9a9f4fd85584fc2ddcda3
Marat <mringer@bk.ru>
2024-02-08 17:56:37 +0500

    бургер и кнопка бургера

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import Nav from '../nav/Nav';
 import Promo from '../promo/promo';
 
 function App() {
+
+  const [showBurger, setShowBurger] = useState(false);
+
+  function burger() {
+    const navMenu = document.querySelector('.nav');
+
+    if (showBurger) {
+      setShowBurger(!showBurger);
+      navMenu.style.left = '-100%';
+
+    } else if (!showBurger) {
+      setShowBurger(!showBurger);
+      navMenu.style.left = '0px';
+    }
+
+  }
+
   return (
     <div className="app">
-      <div className="burger">open</div>
+      <div className={'burger ' + (showBurger ? 'burger__close' : 'burger__open')} onClick={burger}>{showBurger ? 'close' : 'open'}</div>
       <div className="app__wrapper">
         <Nav />
 
